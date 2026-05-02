e8bbcf05109d4ace06558a21fcdc4df059ec5388
sharmad-tadkodkar <sharmadtdk221@gmail.com>
2026-05-02 17:52:58 +0530

    Added login, db and new cards to civicxp

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'TWfGySfS5ECRva4nvdooTIY29WyWWh_s',
+            'X-App-Proxy': '6nJoNmP9tdunW5cl7Cwdb9nw1qSeOGUl',
           },
           body: JSON.stringify(requestDetails),
         };
